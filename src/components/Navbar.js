35a759a884b2d6962f36e126116435d9e63d9ec7
Gregory Quezada <quezada.gregory@yahoo.com>
2022-06-23 01:24:00 -0400

    Added base navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <Link to='/' className='navbar-logo'>
+                    GRG <i className='fab fa-typo3' />
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={ click ? 'fas fa-times' : 'fas fa-bars' } />
+                </div>
+                <ul className={ click ? 'nav-menu active' : 'nav-menu' }>
+                    <li className='nav-item'>
+                        
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </>
+  );
+}
+
+export default Navbar
